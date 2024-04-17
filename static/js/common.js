5ce3078c3314329bd305ca8fd65b7e3bdfc1d9ad
shinew-git <shinew.ibiz@gmail.com>
2024-04-17 13:24:24 +0900

    loading 추가

--- static/js/common.js
+++ static/js/common.js
@@ -1,19 +1,22 @@
 
+// 호출
 function callLaywer(url, contentId, answerId) {
     const answerEl = document.getElementById(answerId)
+    loading(true)
 
     getAnswer(url, contentId).then(res => {
         console.log('res!!!!', res)
         answerEl.innerHTML = 'JSON:' + JSON.stringify(res)
+        loading(false)
     }).catch((error) => {
         alert('답변 중 오류가 발생했습니다.')
         console.error(error)
-    });;
+        loading(false)
+    })
 }
 
-
+// api 연결
 function getAnswer(url, contentId) {
-
     const contents = document.getElementById(contentId).value
     const jsonData = {'input': contents}
 
@@ -28,12 +31,10 @@ function getAnswer(url, contentId) {
         xhr.onload = function () {
         if (xhr.status == 200) {
             //success 
-            // console.log(xhr.response)
             const result = JSON.parse(xhr.response)
             resolve(result)
         } else {
             //failed
-            // console.log('error', xhr)
             const result = JSON.parse(xhr.response)
             reject(result)
         }
@@ -41,3 +42,33 @@ function getAnswer(url, contentId) {
     });
 }
 
+// loading
+function loading(isStart) {
+    const bodyEl = document.getElementsByTagName('body')[0]
+
+    if(isStart) {
+        if(document.getElementById('mask')) return
+
+        const maskHeight = window.document.body.clientHeight;
+        const maskWidth  = window.document.body.clientWidth;
+        const maskEl = document.createElement('div')
+        maskEl.id = 'mask'
+        maskEl.style.cssText = 'width:'+ maskWidth +'; height:'+ maskHeight +'; opacity: 0.3; position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'
+    
+        const imgPath = '/static/images/loading.gif';
+        const loadingEl = document.createElement('img')
+        loadingEl.id = 'loadingImg'
+        loadingEl.src = imgPath
+        loadingEl.style.cssText = 'position: absolute; display: block; margin: 0px auto;'
+
+        bodyEl.append(maskEl)
+        maskEl.style.display = 'block'
+
+        maskEl.append(loadingEl)
+        loadingEl.style.display = 'block'
+    } else {
+        const maskEl = document.getElementById('mask')
+        maskEl?.remove()
+    }
+}
+ 
