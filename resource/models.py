0446644dcefb796b6534b1cc96065a66e2a21050
shinew-git <shinew.ibiz@gmail.com>
2024-04-17 16:18:59 +0900

    주석 추가, full text 컬럼 삭제

--- resource/models.py
+++ resource/models.py
@@ -9,16 +9,27 @@ class Test1(Base):
 
 class Precedent(Base):
     __tablename__ = 'precedent'
+    # 판례정보일련번호
     CaseSerialNumber = Column(BigInteger, primary_key=True)
+    # 사건명
     CaseName = Column(Text)
+    # 사건번호
     CaseNumber = Column(Text)
+    # 선고일자
     JudgmentDate = Column(BigInteger)
+    # 선고
     JudgmentType = Column(Text)
+    # 법원명
     CourtName = Column(Text)
+    # 사건종류명
     CaseType = Column(Text)
+    # 판결유형
     VerdictType = Column(Text)
+    # 판시사항
     Matter = Column(Text)
+    # 판결요지
     Summary = Column(Text)
+    # 참조조문
     ReferenceArticle = Column(Text)
+    # 참조판례
     ReferenceCase = Column(Text)
-    FullText = Column(Text)
