f2df9f5f45ed5794e2acd0793672cf8be5ebbd5a
architectyou <youngkairos98@gmail.com>
2024-04-17 18:35:18 +0900

    test_young.py file 수정

--- service/test_young.py
+++ service/test_young.py
@@ -1,2 +1,26 @@
+# module import
+import torch
+import numpy as np
+import os
+from transformers import AutoTokenizer, AutoModel
+# numpy module
+from numpy import dot
+from numpy.linalg import norm
+
+
+# load model
+tokenizer = AutoTokenizer.from_pretrained('jhgan/ko-sroberta-multitask')
+model = AutoModel.from_pretrained('jhgan/ko-sroberta-multitask')
+
 def testData(input, db):
     return
+
+# Embedding & cosine similarity
+def mean_pooling(model_output, attention_mask) :
+    token_embeddings = model_output[0]
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
+def cos_sim(A, B) :
+    return dot(A, B) / (norm(A)*norm(B))
