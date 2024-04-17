25e30b9743092666b66c657c5c5d25966e04f8d0
shinew-git <shinew.ibiz@gmail.com>
2024-04-17 14:54:53 +0900

    table model 추가

--- resource/models.py
+++ resource/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, BigInteger
 from resource.database import Base
 
 class Test1(Base):
@@ -6,3 +6,19 @@ class Test1(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100))
     content = Column(String(100))
+
+class Precedent(Base):
+    __tablename__ = 'precedent'
+    CaseSerialNumber = Column(BigInteger, primary_key=True)
+    CaseName = Column(Text)
+    CaseNumber = Column(Text)
+    JudgmentDate = Column(BigInteger)
+    JudgmentType = Column(Text)
+    CourtName = Column(Text)
+    CaseType = Column(Text)
+    VerdictType = Column(Text)
+    Matter = Column(Text)
+    Summary = Column(Text)
+    ReferenceArticle = Column(Text)
+    ReferenceCase = Column(Text)
+    FullText = Column(Text)
